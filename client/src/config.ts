a32033a6e689b34284d579684b50c2f994d4a853
ShakerHussien <shakerhussiencmp@gmail.com>
2020-07-03 22:07:02 +0200

    add config to client app

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = ''
+const apiId = '6mjsa064ja'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   //Create an Auth0 application and copy values from it into this map
-  domain: '', // Auth0 domain
-  clientId: '', // Auth0 client id
+  domain: 'dev-shaker.auth0.com', // Auth0 domain
+  clientId: 'pjsj5YuwFFAZipFaF3QjvkOUMnawN0zX', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
